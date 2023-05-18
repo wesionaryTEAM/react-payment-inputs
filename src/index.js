2ff9b0bfbe657c19ac6f6aed4d56c3d8bb028cd0
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2023-05-18 07:46:49 +0545

    chore: move styled component out of defalut

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 export { default as usePaymentInputs } from './usePaymentInputs';
-export { default as PaymentInputsContainer } from './PaymentInputsContainer';
-export { default as PaymentInputsWrapper } from './PaymentInputsWrapper';
 export { getCardNumberError, getExpiryDateError, getCVCError, getZIPError } from './utils/validator';
---
+++ src/styled/index.js
@@ -0,0 +1,2 @@
+export { default as PaymentInputsWrapper } from './PaymentInputsWrapper';
+export { default as PaymentInputsContainer } from './PaymentInputsContainer';
